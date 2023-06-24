7fa82470745b477eec5291ef762cb65a7a86ef34
shadowctrl <shadowctrl@outlook.com>
2023-06-24 10:46:20 +0530

    Routing

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Error from "./error";
+import Main from "./components/main/main";
+import Navbar from "./components/navbar/navbar";
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="*" element={<Error />} />
+        <Route
+          path="/"
+          element={
+            <>
+              {" "}
+              <Navbar />
+              <Main />
+            </>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
